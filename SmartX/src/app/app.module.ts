4273d6e6f958fa6f28e7eeab7fbfb6c67b2d95bf
alan <15030093@itcelaya.edu.mx>
2018-11-11 20:46:38 -0600

    Menus

--- SmartX/src/app/app.module.ts
+++ SmartX/src/app/app.module.ts
@@ -5,15 +5,31 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 //Angular material componentes
 import {Material} from "./Material";
-
+//Graficas
+import {ChartsModule} from "ng2-charts";
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule, MatCardModule, MatMenuModule, MatIconModule, MatButtonModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
+import { GraficasComponent } from './graficas/graficas.component';
+import { GraficaSemanaComponent } from './grafica-semana/grafica-semana.component';
+import { GraficaMesComponent } from './grafica-mes/grafica-mes.component';
+import { GraficaAnioComponent } from './grafica-anio/grafica-anio.component';
+
+//rutas
+import {routing,appRoutingProviders} from "./app.routing";
+import { StoreComponent } from './store/store.component';
+import { BajasComponent } from './bajas/bajas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    GraficasComponent,
+    GraficaSemanaComponent,
+    GraficaMesComponent,
+    GraficaAnioComponent,
+    StoreComponent,
+    BajasComponent
   ],
   imports: [
     BrowserModule,
@@ -24,9 +40,13 @@ import { LayoutModule } from '@angular/cdk/layout';
     MatMenuModule,
     MatIconModule,
     MatButtonModule,
-    LayoutModule
+    LayoutModule,
+    ChartsModule,
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
